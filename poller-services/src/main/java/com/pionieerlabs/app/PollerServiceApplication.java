3aafade6c1e8378cf03db59ff725dfc8cd5d4839
rbiedrawa <biedrawa.rafal@gmail.com>
2017-09-08 15:44:14 +0200

    Added Hystrix Dashboard

--- poller-services/src/main/java/com/pionieerlabs/app/PollerServiceApplication.java
+++ poller-services/src/main/java/com/pionieerlabs/app/PollerServiceApplication.java
@@ -3,9 +3,11 @@ package com.pionieerlabs.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 import com.pionieerlabs.app.config.MessageProperties;
 
+@EnableHystrixDashboard
 @SpringBootApplication
 @EnableConfigurationProperties({MessageProperties.class})
 public class PollerServiceApplication {
