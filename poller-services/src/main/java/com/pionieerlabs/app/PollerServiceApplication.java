a1a9c63004e79205744ecc4d4f8bcc4cacf624c7
rbiedrawa <biedrawa.rafal@gmail.com>
2017-09-08 16:03:33 +0200

    Added Hawtio monitoring dashboard.

--- poller-services/src/main/java/com/pionieerlabs/app/PollerServiceApplication.java
+++ poller-services/src/main/java/com/pionieerlabs/app/PollerServiceApplication.java
@@ -6,13 +6,19 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 import com.pionieerlabs.app.config.MessageProperties;
+import io.hawt.springboot.EnableHawtio;
+import io.hawt.web.AuthenticationFilter;
 
+@EnableHawtio
 @EnableHystrixDashboard
 @SpringBootApplication
 @EnableConfigurationProperties({MessageProperties.class})
 public class PollerServiceApplication {
 
 	public static void main(String[] args) {
+		// FIXME: Only for demo purpose, don't do this in production env
+		System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
+
 		SpringApplication.run(PollerServiceApplication.class, args);
 	}
 }
